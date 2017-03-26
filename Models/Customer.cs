5d879522ed6946f4b2529634b43518760000e887
CCordovaT <ccordovat@CL00TI23>
2017-03-26 12:55:38 -0500

    MVC basico completo

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,14 +1,25 @@
 using Dapper.Contrib.Extensions;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tibox.Models
 {
     [Table("Customer")]
     public class Customer
     {        
+        [ScaffoldColumn(false)] //para que en las vistas no aparesca este campo
         public int Id { get; set; }                
-        public string FirstName { get; set; }        
+
+        [Display(Name = "Primer Nombre")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [StringLength(40, ErrorMessage = "Longitud maxima es 40")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Segundo Nombre")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [StringLength(40, ErrorMessage = "Longitud maxima es 40")]
         public string LastName { get; set; }
+
         public string City { get; set; }
         public string Country { get; set; }
         public string Phone { get; set; }
---
+++ Tibox.Mvc/Controllers/CustomerController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tibox.UnitOfWork;
+using Tibox.Models;
+
+namespace Tibox.Mvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly TiboxUnitOfWork _unit;
+
+        public CustomerController()
+        {
+            _unit = new TiboxUnitOfWork();
+        }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(_unit.Customers.GetAllEntitys());
+        }
+
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+        public PartialViewResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+
+            if (!ModelState.IsValid) return PartialView("Create", customer);
+            var id = _unit.Customers.Insert(customer);
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(_unit.Customers.GetEntityById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Customer customer)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (_unit.Customers.Update(customer))
+                    return RedirectToAction("Index");
+            }
+            return View(customer);
+
+        }
+
+        public ActionResult delete(int id)
+        {
+            return View(_unit.Customers.GetEntityById(id));
+        }
+
+        [HttpPost]
+        public ActionResult delete(Customer customer)
+        {
+            if (_unit.Customers.Delete(customer)) return RedirectToAction("Index");
+            return View(customer);                        
+        }
+
+    }
+}
